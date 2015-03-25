245c7067b8b4f1d7eb68d9ef1d9c9d3289463eca
Andrzej Janik <vosen@vosen.pl>
2015-03-26 00:11:31 +0100

    Add custom action for alerting about build failures

---
+++ BuildAlert/BuildAlert/Background.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildAlert
+{
+    public class Background
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SystemParametersInfo(int uAction,
+            int uParam, string lpvParam, int fuWinIni);
+ 
+        private static readonly int MAX_PATH = 260;
+        private static readonly int SPI_GETDESKWALLPAPER = 0x73;
+        private static readonly int SPI_SETDESKWALLPAPER = 0x14;
+        private static readonly int SPIF_UPDATEINIFILE = 0x01;
+        private static readonly int SPIF_SENDWININICHANGE = 0x02;
+ 
+        public static string Get()
+        {
+            string wallpaper = new string('\0', MAX_PATH);
+            SystemParametersInfo(SPI_GETDESKWALLPAPER, (int)wallpaper.Length, wallpaper, 0);
+            return wallpaper.Substring(0, wallpaper.IndexOf('\0'));
+        }
+ 
+        public static void Set(string filename)
+        {
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, filename,
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+    }
+}
---
+++ BuildAlert/BuildAlert/FailureAlert.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BuildAlert
+{
+    public class FailureAlert : Task
+    {
+        public override bool Execute()
+        {
+            Background.Set(@"D:\Users\vosen\Documents\Visual Studio 2013\Projects\MotoStudio\Examples\Targets\fail.png");
+            return true;
+        }
+    }
+}
